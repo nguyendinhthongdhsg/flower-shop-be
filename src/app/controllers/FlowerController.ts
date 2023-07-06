8b91b262c5a805f08fb648f1806e16097311ca0f
Nguyen Dinh Thong <nguyenthong244466666@gmail.com>
2023-07-06 19:21:12 +0700

    Flower Controller

--- src/app/controllers/FlowerController.ts
+++ src/app/controllers/FlowerController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import Flower from '../models/Flower';
 import Image from '../models/Image';
+import Directory from '../models/Directory';
 
 class FlowerController {
     post(req: Request, res: Response, next: NextFunction) {
@@ -9,38 +10,64 @@ class FlowerController {
             price: Number(req.body.price),
             id: req.body.id,
         };
-        const fileReq = {
-            id: req.body.id,
-            data: req.file?.buffer,
-            contentType: req.file?.mimetype,
-        };
-        Flower.findOne({ name: formReq.name })
-            .then((flowerFind) => {
-                if (flowerFind?.name) {
-                    res.json({ error: 'Tên sản phẩm đã tồn tại' });
-                } else {
-                    Flower.findOne({ id: formReq.id })
-                        .then((flowerFind1) => {
-                            if (flowerFind1?.id) {
-                                res.json({ error: 'ID sản phẩm đã tồn tại' });
-                            } else {
-                                const formDB = new Flower(formReq);
-                                const imageDB = new Image(fileReq);
-                                formDB
-                                    .save()
-                                    .then(() => {
-                                        imageDB
-                                            .save()
-                                            .then(() => res.json({ success: true }))
-                                            .catch(next);
-                                    })
-                                    .catch(next);
-                            }
-                        })
-                        .catch(next);
-                }
-            })
-            .catch(next);
+        if (formReq.id.indexOf(' ') === -1) {
+            res.json({
+                error: `ID sản phẩm phải chứa mã ID danh mục và ID sản phẩm (VD: ${formReq.id} 01)`,
+            });
+        } else {
+            const fileReq = {
+                id: req.body.id,
+                data: req.file?.buffer,
+                contentType: req.file?.mimetype,
+            };
+            Flower.findOne({ name: formReq.name })
+                .then((flowerFind) => {
+                    if (flowerFind?.name) {
+                        res.json({ error: 'Tên sản phẩm đã tồn tại' });
+                    } else {
+                        Flower.findOne({ id: formReq.id })
+                            .then((flowerFind1) => {
+                                if (flowerFind1?.id) {
+                                    res.json({ error: 'ID sản phẩm đã tồn tại' });
+                                } else {
+                                    let checkDir = '';
+                                    const stringId = formReq.id;
+                                    const length = stringId.length;
+                                    let i = 0;
+                                    while (stringId) {
+                                        if (stringId[i] === ' ' || i === length) break;
+                                        checkDir += stringId[i];
+                                        i++;
+                                    }
+                                    Directory.findOne({ id: checkDir })
+                                        .then((dirFind) => {
+                                            if (dirFind?.name) {
+                                                const formDB = new Flower(formReq);
+                                                const imageDB = new Image(fileReq);
+                                                formDB
+                                                    .save()
+                                                    .then(() => {
+                                                        imageDB
+                                                            .save()
+                                                            .then(() =>
+                                                                res.json({ success: true, formDB })
+                                                            )
+                                                            .catch(next);
+                                                    })
+                                                    .catch(next);
+                                            } else if (!dirFind || !dirFind.name)
+                                                res.json({
+                                                    error: `Danh mục ${checkDir} chưa được khởi tạo`,
+                                                });
+                                        })
+                                        .catch(next);
+                                }
+                            })
+                            .catch(next);
+                    }
+                })
+                .catch(next);
+        }
     }
 
     get(req: Request, res: Response, next: NextFunction) {
